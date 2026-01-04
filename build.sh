26b1504e7d1a4125918d3d2b5ac67f17b7e72ee9
Ethan <SirEthanator@users.noreply.github.com>
2026-01-04 16:51:50 +1300

    Improve build.sh and remove generate.sh
    * Removed generate.json
    * Take arguments in gen_render_json.sh
    * Run cbmp in build.sh
    * Add --skip-xcursors, --skip-hyprcursors, --skip-windows, and
      --skip-bitmaps to build.sh
    * Slightly improve error handling in build.sh
    * Improve output of build.sh
    * Remove useless waits from build.sh
    * Use a proper temp directory for hyprcursor generation

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-# A script for preparing binaries of Bibata Cursors, created by Abdulkaiz Khatri, Modified by SirEthanator.
-
-# TODO: Skip windows generation flag
+# A script for preparing binaries of Bibata Cursors, created by Abdulkaiz Khatri, modified by SirEthanator.
 
 version="v2.0.6"
 
-error() (
-  set -o pipefail
-  "$@" 2> >(sed $'s,.*,\e[31m&\e[m,' >&2)
-)
+error() {
+  printf "\033[0;31mERROR: \033[0m%s\n" "$1"
+  if [[ "$#" -gt 1 ]]; then
+    shift
+    for msg in "${@}"; do
+      echo "$msg"
+    done
+  fi
+  exit 1
+}
+
+warn() {
+  printf "\033[0;33mWARN: \033[0m%s\n" "$1"
+}
 
 get_config_path() {
   local key="${1}"
@@ -28,93 +36,166 @@ with_version() {
   echo "$comment ($version)."
 }
 
-if ! type -p ctgen >/dev/null; then
-  error ctgen
-  exit 127 # exit program with "command not found" error code
-fi
+declare -A allNames
+allNames["Bibata-Modern-Everforest"]=$(with_version "Everforest and rounded edge Bibata")
+allNames["Bibata-Modern-Everforest-Light"]=$(with_version "Light Everforest and rounded edge Bibata")
+allNames["Bibata-Modern-CatMocha"]=$(with_version "Catppuccin Mocha and rounded edge Bibata")
+allNames["Bibata-Modern-CatLatte"]=$(with_version "Catppuccin Latte and rounded edge Bibata")
+allNames["Bibata-Modern-Rose-Pine"]=$(with_version "Rose Pine and rounded edge Bibata")
+allNames["Bibata-Modern-Rose-Pine-Dawn"]=$(with_version "Rose Pine Dawn and rounded edge Bibata")
+allNames["Bibata-Modern-Material"]=$(with_version "Material Dark and rounded edge Bibata")
+allNames["Bibata-Modern-Material-Light"]=$(with_version "Material Light and rounded edge Bibata")
 
-declare -A names
-names["Bibata-Modern-Everforest"]=$(with_version "Everforest and rounded edge Bibata")
-names["Bibata-Modern-Everforest-Light"]=$(with_version "Light Everforest and rounded edge Bibata")
-names["Bibata-Modern-CatMocha"]=$(with_version "Catppuccin Mocha and rounded edge Bibata")
-names["Bibata-Modern-CatLatte"]=$(with_version "Catppuccin Latte and rounded edge Bibata")
-names["Bibata-Modern-Rose-Pine"]=$(with_version "Rose Pine and rounded edge Bibata")
-names["Bibata-Modern-Rose-Pine-Dawn"]=$(with_version "Rose Pine Dawn and rounded edge Bibata")
-names["Bibata-Modern-Material"]=$(with_version "Material Dark and rounded edge Bibata")
-names["Bibata-Modern-Material-Light"]=$(with_version "Material Light and rounded edge Bibata")
-
-if [[ "$#" -gt 0 ]]; then
-  declare -A tmp
-  for key in "$@"; do [[ -v names[$key] ]] && tmp[$key]="${names[$key]}"; done
-
-  unset names
-  declare -A names
-  for key in "${!tmp[@]}"; do
-    names[$key]="${tmp[$key]}"
+is_valid_name() {
+  local name="$1"
+  for key in "${!allNames[@]}"; do
+    if [[ "$name" == "$key" ]]; then
+      return 0
+    fi
   done
 
-  unset tmp
+  return 1
+}
+
+declare -a names
+
+skip_windows=false
+skip_hyprcursors=false
+skip_xcursors=false
+
+skip_bitmaps=false
+
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+  --skip-windows)
+    skip_windows=true
+    ;;
+  --skip-hyprcursors)
+    skip_hyprcursors=true
+    ;;
+  --skip-xcursors)
+    skip_xcursors=true
+    ;;
+  --skip-bitmaps)
+    skip_bitmaps=true
+    ;;
+  *)
+    if is_valid_name "$1"; then
+      names+=("$1")
+    else
+      error "Invalid option: ${1}"
+    fi
+    ;;
+  esac
+  shift
+done
+
+if [[ ${#names[@]} -eq 0 ]]; then
+  for key in "${!allNames[@]}"; do
+    names+=("$key")
+  done
+fi
+
+if ! which ctgen >/dev/null 2>&1; then
+  error 'ctgen not found.' 'Please install it from https://github.com/ful1e5/clickgen'
+fi
+
+if [[ $skip_bitmaps = false ]] && ! which cbmp >/dev/null 2>&1; then
+  error 'cbmp-rs not found.' 'Please install it from https://github.com/SirEthanator/cbmp-rs'
+fi
+
+if [[ $skip_hyprcursors == false ]] && [[ $skip_xcursors == true ]]; then
+  error 'Hyprcursor generation requires xcursors to be generated first.' 'To skip XCursors, also skip hyprcursors.'
+fi
+
+# Generate bitmaps
+if [[ $skip_bitmaps == false ]]; then
+  echo 'Generating bitmaps...'
+  ./gen_render_json.sh "${names[@]}"
+  cbmp --quiet ./render.json
 fi
 
 # Cleanup old builds
-for key in "${!names[@]}"; do
-  rm -rf themes/"$key"
-  rm -f bin/"${key}.tar.xz"
+for key in "${names[@]}"; do
+  # If XCursors are skipped, so are hyprcursors
+  if [[ $skip_xcursors == false ]]; then
+    echo "Cleaning old XCursors and hyprcursors (${key})..."
+    rm -rf themes/"$key"
+    rm -f bin/"${key}.tar.xz"
+    echo "Done"
+  fi
+
+  if [[ $skip_windows == false ]]; then
+    echo "Cleaning old Windows cursors (${key})..."
+    rm -rf themes/"${key}-Windows"
+    rm -f bin/"${key}-Windows.tar.xz"
+    echo "Done"
+  fi
 done
 
 # Building Bibata XCursor binaries
-for key in "${!names[@]}"; do
-  comment="${names[$key]}"
-  cfg_path=$(get_config_path "$key")
+if [[ $skip_xcursors == false ]]; then
+  for key in "${names[@]}"; do
+    comment="${allNames[$key]}"
+    cfg_path=$(get_config_path "$key")
 
-  ctgen "$cfg_path/x.build.toml" -p x11 -d "bitmaps/$key" -n "$key" -c "$comment XCursors" &
-  PID=$!
-  wait $PID
-done
+    ctgen "$cfg_path/x.build.toml" -p x11 -d "bitmaps/${key}" -n "$key" -c "$comment XCursors"
+  done
+fi
 
 # Building Bibata Windows binaries
-for key in "${!names[@]}"; do
-  comment="${names[$key]}"
-  cfg_path=$(get_config_path "$key")
+if [[ $skip_windows == false ]]; then
+  for key in "${names[@]}"; do
+    comment="${allNames[$key]}"
+    cfg_path=$(get_config_path "$key")
 
-  ctgen "$cfg_path/win_rg.build.toml" -d "bitmaps/$key" -n "$key-Regular" -c "$comment Windows Cursors" &
-  ctgen "$cfg_path/win_lg.build.toml" -d "bitmaps/$key" -n "$key-Large" -c "$comment Windows Cursors" &
-  ctgen "$cfg_path/win_xl.build.toml" -d "bitmaps/$key" -n "$key-Extra-Large" -c "$comment Windows Cursors" &
-  PID=$!
-  wait $PID
-done
+    ctgen "$cfg_path/win_rg.build.toml" -d "bitmaps/$key" -n "$key-Regular" -c "$comment Windows Cursors"
+    ctgen "$cfg_path/win_lg.build.toml" -d "bitmaps/$key" -n "$key-Large" -c "$comment Windows Cursors"
+    ctgen "$cfg_path/win_xl.build.toml" -d "bitmaps/$key" -n "$key-Extra-Large" -c "$comment Windows Cursors"
+  done
+fi
 
 # Generate Hyprcursors and compress binaries
 mkdir -p bin
-cd themes || exit
+cd themes || exit 1
 
-for key in "${!names[@]}"; do
-  mkdir tmp
-  hyprcursor-util -x "$key" -o tmp
-  hyprcursor-util -c tmp/extracted* -o tmp
-  mv 'tmp/theme_Extracted Theme'/* "$key"
-  rm -rf tmp
-  tar -cJvf "../bin/${key}.tar.xz" "${key}" &
-  PID=$!
-  wait $PID
+for key in "${names[@]}"; do
+  if [[ $skip_hyprcursors == false ]]; then
+    echo "Generating hyprcursors (${key})..."
+
+    {
+      tmpdir=$(mktemp -d '/tmp/bibata_hyprcursor_XXXXXX')
+      hyprcursor-util -x "$key" -o "$tmpdir"
+      hyprcursor-util -c "$tmpdir"/extracted* -o "$tmpdir"
+      mv "$tmpdir"/theme_Extracted Theme/* "$key"
+      rm -rf "$tmpdir"
+    } >/dev/null 2>&1 || warn "Hyprcursor generation failed"
+
+    echo "Done"
+  fi
+
+  [[ $skip_hyprcursors == false ]] && hyprcursor_str=" and hyprcursors" || hyprcursor_str=""
+  echo "Adding XCursors${hyprcursor_str} to archive (${key})..."
+  tar -cJf "../bin/${key}.tar.xz" "${key}" >/dev/null 2>&1
+  echo "Done"
 done
-
-# Compressing Bibata.tar.xz
-cp ../LICENSE .
-tar -cJvf "../bin/Bibata.tar.xz" --exclude="*-Windows" . &
-PID=$!
-wait $PID
 
 # Compressing Bibata-*-Windows
-for key in "${!names[@]}"; do
-  zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" &
-  PID=$!
-  wait $PID
-done
+if [[ $skip_windows == false ]]; then
+  for key in "${names[@]}"; do
+    echo "Zipping Windows cursors (${key})..."
+    zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" >/dev/null 2>&1
+    echo "Done"
+  done
+fi
 
 cd ..
 
-# Copying License File for 'bitmaps'
-cp LICENSE bitmaps/
-zip -rv bin/bitmaps.zip bitmaps
+if [[ $skip_bitmaps == false ]]; then
+  # Copying License File for 'bitmaps'
+  cp LICENSE bitmaps/
 
+  echo "Zipping bitmaps..."
+  zip -rv bin/bitmaps.zip bitmaps >/dev/null 2>&1
+  echo "Done"
+fi
--- gen_render_json.sh
+++ gen_render_json.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-jq -s 'reduce .[] as $item ({}; . * $item)' schemes/* > render.json
+ROOT="$( cd -- "$(dirname "$0")" || exit 1 > /dev/null 2>&1 ; pwd -P )"
 
+declare -a files
+
+if [[ "$#" -lt 1 ]]; then
+  echo "No schemes provided. Adding all schemes..."
+  files=("$ROOT"/schemes/*)
+else
+  for scheme in "$@"; do
+    path="${ROOT}/schemes/${scheme}.json"
+    if [[ -e "$path" ]]; then
+      files+=("$path")
+    else
+      echo "Warning: Scheme not found: ${scheme}. Skipping..."
+    fi
+  done
+fi
+
+if [[ "${#files}" -lt 1 ]]; then
+  echo "Error: Nothing to do. Please provide at least one valid scheme."
+  exit 1
+fi
+
+jq -s 'reduce .[] as $item ({}; . * $item)' "${files[@]}" > render.json
